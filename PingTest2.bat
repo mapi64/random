f1439c4aff0c31b0de0829b822b8a288e9b92b6d
mapi64-desktop <66664444@protonmail.com>
2021-09-29 22:14:18 +0200

    123

---
+++ PingTest2.bat
@@ -0,0 +1,17 @@
+cd %temp%
+echo %computername% %date% %time% >> 0001Pingtest.txt
+ping google.com >> 0001Pingtest.txt
+echo '                                         ' >> 0001Pingtest.txt
+echo -------------------------------------------------------- >> 0001Pingtest.txt
+echo %computername% %date% %time% >> 0001Pingtest.txt
+ping office.com >> 0001Pingtest.txt
+echo '                                         ' >> 0001Pingtest.txt
+echo -------------------------------------------------------- >> 0001Pingtest.txt
+echo %computername% %date% %time% >> 0001Pingtest.txt
+ping svt.com >> 0001Pingtest.txt
+echo '                                         ' >> 0001Pingtest.txt
+echo -------------------------------------------------------- >> 0001Pingtest.txt
+echo %computername% %date% %time% >> 0001Pingtest.txt
+ping ngo.visolit.se >> 0001Pingtest.txt
+echo '                                         ' >> 0001Pingtest.txt
+echo -------------------------------------------------------- >> 0001Pingtest.txt
